24c6e7a01afc82609d23294795ca4a3dc1d0ed9c
vitor souza <vitor.engineer2811@outlook.com>
2024-06-29 11:41:16 -0300

    if/else

---
+++ index.js
@@ -0,0 +1,11 @@
+
+let idade = Number (prompt('insira sua idade'))
+// == igual ! diferente  
+if(idade <=10){ 
+    console.log('voce tem menos ou tem 10 anos')
+
+
+}else{
+    console.log('voce tem mais que 10 anos')
+
+}
